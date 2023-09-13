181bed8475638f431737be99ffe103284129f57c
Vladyslav Staryy <staryyvladyslav@gmail.com>
2023-09-13 23:29:48 +0300

    panel win check 1

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -3,6 +3,7 @@ using Unity.Netcode;
 using UnityEngine.UI;
 using System;
 using TMPro;
+using System.Collections.Generic;
 
 namespace Assets.Scripts
 {
@@ -10,6 +11,7 @@ namespace Assets.Scripts
     {
         private Button[,] _buttons = new Button[9, 9];
         [SerializeField] private TextMeshProUGUI _joinCodeText;
+        int [,] win = { {0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6} };
         public override void OnNetworkSpawn()
         {
             _joinCodeText.text = GameManager.JoinCode;
@@ -49,7 +51,48 @@ namespace Assets.Scripts
                 _buttons[r, c].interactable = false;
                 ChangeSpriteServerRpc(r, c);
             }
+            CheckPanelForWin(r);
+        }
 
+        private void CheckPanelForWin(int panelIndex)
+        {
+            List<Sprite> panelSprites = new List<Sprite>();
+            var panel = gameObject.transform.GetChild(0).GetChild(panelIndex).gameObject;
+            var buttons = panel.GetComponentsInChildren<Image>();
+            for(int j = 1; j < 10; j++)
+            {
+                panelSprites.Add(buttons[j].sprite);
+            }
+            for(int i = 0; i < win.Length/3; i++)
+            {
+                if(panelSprites[win[i,0]].name != "Background" && panelSprites[win[i,0]].name == panelSprites[win[i, 1]].name && panelSprites[win[i, 1]].name == panelSprites[win[i, 2]].name)
+                {
+                    for(int a = 1; a < buttons.Length; a++)
+                    {
+                        Destroy(buttons[a].gameObject);
+                    }
+                    
+                    if(GameManager.Instance.CurrentTurn.Value == 1)
+                    {
+                        panel.GetComponent<Image>().sprite = xSprite;
+                        ChangePanelSpriteClientRpc(panelIndex);
+                    } else 
+                    {
+                        panel.GetComponent<Image>().sprite = oSprite;
+                        ChangePanelSpriteServerRpc(panelIndex);
+                    }
+                    break;
+                }
+            }
+        }
+
+        [ClientRpc]
+        private void ChangePanelSpriteClientRpc(int panelIndex){
+            gameObject.transform.GetChild(0).GetChild(panelIndex).GetComponent<Image>().sprite = xSprite;
+        }
+        [ServerRpc]
+        private void ChangePanelSpriteServerRpc(int panelIndex){
+            gameObject.transform.GetChild(0).GetChild(panelIndex).GetComponent<Image>().sprite = oSprite;
         }
 
         [ClientRpc]
